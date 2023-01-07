8111adb00f405a9b01f53df2b2a588e40d0f17d7
pearapeepat1 <peelover25@gmail.com>
2023-01-07 11:35:36 +0700

    edit2

--- connect.php
+++ connect.php
@@ -5,11 +5,10 @@ $password = "it65309010106";
 $dbname = "it65309010106";
 
 // Create connection
-$conn = mysqli_connect("$servername", "$username", "$password", "$dbname");
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8");
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}else{
-    //echo"<h1> connect suuccessfull</h1>";
+die("Connection failed: " . mysqli_connect_error());
 }
 ?>
